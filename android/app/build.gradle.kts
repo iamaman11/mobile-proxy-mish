1fd3de9e2ac2718cdf8367d32007b123ae52f54a
iamaman11 <44100369+iamaman11@users.noreply.github.com>
2026-09-09 13:52:49 +0300

    B2 E3: add Android instrumentation build contract

--- android/app/build.gradle.kts
+++ android/app/build.gradle.kts
@@ -96,6 +96,7 @@ android {
         targetSdk = 36
         versionCode = 1
         versionName = "0.1.0-dev"
+        testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
 
         ndk {
             abiFilters += "arm64-v8a"
@@ -134,4 +135,7 @@ dependencies {
     implementation("net.java.dev.jna:jna:5.19.1@aar")
 
     testImplementation("junit:junit:4.13.2")
+
+    androidTestImplementation("androidx.test:runner:1.7.0")
+    androidTestImplementation("androidx.test.ext:junit:1.3.0")
 }
