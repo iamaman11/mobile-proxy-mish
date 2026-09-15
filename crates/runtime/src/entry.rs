31471d8f17ae90331186699f26a2052f72e3f3bc
iamaman11 <44100369+iamaman11@users.noreply.github.com>
2026-09-16 01:53:46 +0300

    Export temporary bridge connector

--- crates/runtime/src/entry.rs
+++ crates/runtime/src/entry.rs
@@ -13,5 +13,8 @@ pub use cellular_connector::*;
 mod cellular_runtime;
 pub use cellular_runtime::*;
 
+mod private_bridge_connector;
+pub use private_bridge_connector::*;
+
 mod proxy_runtime;
 pub use proxy_runtime::*;
