8ce07933663abeb56296affa1017c6b9388d5eb8
iamaman11 <44100369+iamaman11@users.noreply.github.com>
2026-09-16 04:30:08 +0300

    L8 export Mesh serving composition policy

--- crates/runtime/src/entry.rs
+++ crates/runtime/src/entry.rs
@@ -13,6 +13,9 @@ pub use cellular_connector::*;
 mod cellular_runtime;
 pub use cellular_runtime::*;
 
+mod mesh_serving;
+pub use mesh_serving::*;
+
 mod proxy_recovery;
 pub use proxy_recovery::*;
 
