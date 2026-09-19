e9439c81c2c502dce0f9fa3509973b34ce7c550a
iamaman11 <44100369+iamaman11@users.noreply.github.com>
2026-09-19 05:41:17 +0300

    fix: wire credential E Kotlin imports

--- android/app/src/main/java/com/mobileproxymish/app/MainActivity.kt
+++ android/app/src/main/java/com/mobileproxymish/app/MainActivity.kt
@@ -8,6 +8,7 @@ import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.Button
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
